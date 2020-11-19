311da427b68f2e7046f63659cb817fd47dfa2bb4
mathieutranchida <mathieu.tranchida@gmail.com>
2020-11-19 15:26:36 -0500

    frontend-framework-complete done

---
+++ client/src/components/headerfolder/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <>
+      <Wrapper>Header</Wrapper>
+    </>
+  );
+};
+
+const Wrapper = styled.div``;
+
+export default Header;
