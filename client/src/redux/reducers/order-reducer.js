ea81bf3235c580969226f01ac69094a69a18dce1
mathieutranchida <mathieu.tranchida@gmail.com>
2020-11-20 16:22:48 -0500

    redux-basic-framework-done

---
+++ client/src/redux/reducers/order-reducer.js
@@ -0,0 +1,11 @@
+const initialState = {
+  status: "idle",
+};
+
+export default function orderReducer(state = initialState, action) {
+  switch (action.type) {
+    default: {
+      return state;
+    }
+  }
+}
