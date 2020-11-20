007bc96988a5eedf19b6073c357924e411857a75
mathieutranchida <mathieu.tranchida@gmail.com>
2020-11-20 14:35:41 -0500

    format-update-homepage

--- client/src/components/homepagefolder/Homepage.js
+++ client/src/components/homepagefolder/Homepage.js
@@ -2,55 +2,51 @@ import React from "react";
 import styled from "styled-components";
 //rfce && rxr
 
-import COLORS from '../../constants';
+import COLORS from "../../constants";
 
-import items from './items.json';
+import items from "./items.json";
 
 const Homepage = () => {
-
-  const itemsList = items.map((item) =>{
-  const quantitiesMessage = item.numInStock === 0 ? 'Unavailable' : `In Stock: ${item.numInStock}`;
-  return (
-  <Item key={item._id}>   
-    <Pic src={item.imageSrc}/>
-    <ProductDetailArea>
-      <Name>{item.name}</Name>
-      <ProductDetail>
-        <span>{`Category: ${item.category}`}</span>
-        {<Stock qty={item.numInStock}>{quantitiesMessage}</Stock>}
-        <span>{`Price: ${item.price}`}</span>
-      </ProductDetail>
-      <PurchaseBtn>
-        Purchase
-      </PurchaseBtn>
-    </ProductDetailArea>
-  </Item>)
-});
+  const itemsList = items.map((item) => {
+    const quantitiesMessage =
+      item.numInStock === 0 ? "Unavailable" : `In Stock: ${item.numInStock}`;
+    return (
+      <Item key={item._id}>
+        <Pic src={item.imageSrc} />
+        <ProductDetailArea>
+          <Name>{item.name}</Name>
+          <ProductDetail>
+            <span>{`Category: ${item.category}`}</span>
+            {<Stock qty={item.numInStock}>{quantitiesMessage}</Stock>}
+            <span>{`Price: ${item.price}`}</span>
+          </ProductDetail>
+          <PurchaseBtn>Purchase</PurchaseBtn>
+        </ProductDetailArea>
+      </Item>
+    );
+  });
   return (
     <>
-      <Wrapper>
-        {itemsList}
-      </Wrapper>
+      <Wrapper>{itemsList}</Wrapper>
     </>
   );
 };
 
 const Stock = styled.span`
-  color: ${props => {
-    if(props.qty === 0){
-      return COLORS.red
-    } else if(props.qty < 5 && props.qty > 0){
-      return COLORS.orange
-    } else{
-      return COLORS.yellowgreen
+  color: ${(props) => {
+    if (props.qty === 0) {
+      return COLORS.red;
+    } else if (props.qty < 5 && props.qty > 0) {
+      return COLORS.orange;
+    } else {
+      return COLORS.yellowgreen;
     }
-    }}
+  }};
 `;
 
 const Pic = styled.img`
   width: 100px;
   height: 100px;
-
 `;
 
 const PurchaseBtn = styled.button`
@@ -60,10 +56,10 @@ const PurchaseBtn = styled.button`
   padding: 10px;
   border: none;
 
-  :hover{
-    box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
-    -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+  :hover {
+    box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
+    -moz-box-shadow: 10px 10px 5px 0px rgba(0, 0, 0, 0.75);
   }
 `;
 
@@ -72,23 +68,23 @@ const Name = styled.span`
 `;
 
 const ProductDetailArea = styled.div`
-  display:flex;
+  display: flex;
   flex-direction: column;
   margin-left: 10px;
   justify-content: space-around;
 `;
 const ProductDetail = styled.div`
-  display:flex;
-   flex-direction:column;
-    margin-top:10px;
-    margin-bottom:10px;
+  display: flex;
+  flex-direction: column;
+  margin-top: 10px;
+  margin-bottom: 10px;
 `;
 
 const Wrapper = styled.div`
   display: grid;
   grid-template-columns: auto auto auto;
   padding: 10px;
-  
+
   margin-right: 20px;
 `;
 
@@ -102,11 +98,7 @@ const Item = styled.div`
   border: 1px none ${COLORS.black};
   border-radius: 10px;
   justify-content: center;
-  align-items:center;
-
-  
-
-
+  align-items: center;
 `;
 
 export default Homepage;
